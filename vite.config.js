20911f93bd3892c9a53d1f1be5144433ec6b30bc
riteshalone04 <riteshalone04@gmail.com>
2026-05-10 13:15:46 +0530

    Fix: Add base path for Protfolio repo

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-  ],
-  base: "/Protfolio/",
+  plugins: [react(), tailwindcss()],
+  base: "/Protfolio/", // Use "portfolio" here because that is your repo name
 })
-
