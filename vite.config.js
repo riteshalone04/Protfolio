e760269831d79f14bd888eaa4668beb72afa6dc6
riteshalone04 <riteshalone04@gmail.com>
2026-05-10 13:01:55 +0530

    df

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Protfolio/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
 })
-export default defineConfig({
-  base: '/your-repo-name/', // Must start and end with a slash /
-  plugins: [react()],
-})
+
